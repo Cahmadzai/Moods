5ea22791ac7a53ba068da3813feb3a5db77ca300
Cahmadzai <crahmadzai@gmail.com>
2022-12-13 13:56:43 -0800

    Implemented connecting to database with flask app.

--- model.py
+++ model.py
@@ -47,3 +47,20 @@ class Mood(db.Model):
 
     def __repr__(self):
         return f'<Mood mood_id={self.mood_id} mood_type={self.mood_type}>'
+
+
+def connect_to_db(flask_app, db_uri="postgresql:///model", echo=True):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print("Connected to the db!")
+
+
+if __name__ == "__main__":
+    from server import app
+
+    connect_to_db(app)
