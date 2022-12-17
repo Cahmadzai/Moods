1fbedf9640bf5645e79fce9ff6097dc4e7eb775d
Cahmadzai <crahmadzai@gmail.com>
2022-12-16 21:03:09 -0800

    Cleaned up code.

--- seed_database.py
+++ seed_database.py
@@ -14,14 +14,12 @@ os.system('createdb model')
 model.connect_to_db(server.app)
 model.db.create_all()
 
-# create fake users here
-# grab user ids here
-# user_ids = []
 
-#Load users data from JSON file
+
+# Load users data from JSON file
 with open('data/users.json') as f:
     users_data = json.loads(f.read())
-    #created empty list 
+    # Created empty list 
     users_in_db = []
     for user in users_data:
         user_handle, email, password = (
@@ -36,8 +34,6 @@ with open('data/users.json') as f:
     model.db.session.commit()
 
 
-# create fake moods here
-# grab mood ids here
 with open('data/moods.json') as f:
     moods_data = json.loads(f.read())
     moods_in_db = []
@@ -55,26 +51,21 @@ with open('data/moods.json') as f:
 with open('data/status_posts.json') as f:
     status_post_data = json.loads(f.read())
 
-# Create status posts, store them in list so we can use them
-# to create fake moods later
+# Create status posts, store them in list 
 status_posts_in_db = []
 
 for status_posts in status_post_data:
-    # get the user_handle, status_description, and mood_type from the status post.
+    # Get the status_description from the status post.
     status_description = status_posts['status_description']
-        # status_posts['mood_type'],
-        # status_posts['mood_id'],
-        # status_posts['user_id'],
-    
-    # dictionary. Then, get the post_create_date and convert it to a
-    # datetime object with datetime.strptime
+    # Getting post_create_date and converting it to a
+    # Datetime object with datetime.strptime
+    # For now using datetime.now()
     post_create_date = datetime.now() # datetime.strptime(status_posts["post_create_date"], "%Y-%m-%d-%H")
-   
 
-    #pulling random user id and mood id from users created in db
+    # Pulling random user id and mood id from users created in db
     random_user_id = choice(users_in_db).user_id
     random_mood_id = choice(moods_in_db).mood_id
-    # create a status here and append it to status_posts_in_db
+    # Create a status here and append it to status_posts_in_db
     db_status_post = crud.create_status(random_user_id, status_description, post_create_date, random_mood_id)
     status_posts_in_db.append(db_status_post)
 
