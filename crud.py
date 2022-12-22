de3d88081311bca016f353a74f586dad354e5167
Cahmadzai <crahmadzai@gmail.com>
2022-12-21 19:29:40 -0800

    Created get_mood_type.

--- crud.py
+++ crud.py
@@ -19,14 +19,12 @@ def get_user_statuses(user_id):
     #double check this, might be adding time expiration in the future or date separation
     return Status.query.filter(Status.user_id == user_id).all()
 
-
 def create_status(user_id, status_description, post_create_date, mood_id):
     """Create and return a new mood status post."""
     status_post = Status(user_id=user_id, status_description=status_description, 
     post_create_date=post_create_date, mood_id=mood_id) 
 
     return status_post
-
 
 def get_all_status_posts():
     """Return all status posts."""
@@ -37,6 +35,14 @@ def create_mood(mood_type):
     mood = Mood(mood_type=mood_type)
 
     return mood
+
+def get_mood_type(mood_type):
+    """Returns the mood for a given mood type"""
+    return Mood.query.filter(Mood.mood_type == mood_type).first()
+
+
+
+
 
 
 
